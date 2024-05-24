fbe44596553b60a1d5e0e6c2a984269d8c830509
Broderick Bentil <bentilfrederick4@gmail.com>
2024-05-24 11:05:57 +0000

    Media Query Partially Completed

--- src/index.js
+++ src/index.js
@@ -3,75 +3,78 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import dark_logo from './asset/logo-dark.svg';
 import light_logo from './asset/logo-light.svg';
+import animationIcon from './asset/icon-animation.svg';
+import designIcon from './asset/icon-design.svg';
+import photographyIcon from './asset/icon-photography.svg';
+import cryptoIcon from './asset/icon-crypto.svg';
+import businessIcon from './asset/icon-business.svg';
 
 function LandingPage() {
   return (
     <section>
-      <div className="nav-bar">
-        <img src={dark_logo} alt="dark logo" />
-        <button className="">Get Started</button>
-      </div>
-
-      <div className="max-skill">
-        <div className="max-skill-left">
-          <h3>Maximize skill, minimize budget</h3>
-          <p>
-            Our modern courses across a range of in-demand skills will give you
-            the knowledge you need to live the life you want.
-          </p>
-          <button className="">Get Started</button>
+      <nav>
+        <div className="nav-bar">
+          <img src={dark_logo} alt="dark logo" />
+          <button className="btn-1">Get Started</button>
         </div>
-        <div className="max-skill-right"></div>
-      </div>
+      </nav>
 
-      {/* Middle sections */}
+      <span>
+        <div className="max-skill">
+          <div className="max-skill-left">
+            <h3>Maximize skill, minimize budget</h3>
+            <p>
+              Our modern courses across a range of in-demand skills will give
+              you the knowledge you need to live the life you want.
+            </p>
+            <button className="">Get Started</button>
+          </div>
+          <div className="max-skill-right"></div>
+        </div>
+      </span>
+
       <main>
         <div className="main-cont">
-          <div className="card">
-            <div className="cards">
-              <FirstCard className="cards" />
-            </div>
-            <SecondCard
-              className="s-cards"
-              img={secondCard.img}
-              title={secondCard.title}
-              message={secondCard.message}
-              getStarted={secondCard.getStarted}
-            />
+          <FirstCard className="all-cards" />
 
-            <SecondCard
-              img={thirdCard.img}
-              title={thirdCard.title}
-              message={thirdCard.message}
-              getStarted={thirdCard.getStarted}
-            />
-          </div>
-          <div className="card">
-            <SecondCard
-              img={fourthCard.img}
-              title={fourthCard.title}
-              message={fourthCard.message}
-              getStarted={fourthCard.getStarted}
-            />
+          <SecondCard
+            className="s-cards"
+            img={secondCard.img}
+            title={secondCard.title}
+            message={secondCard.message}
+            getStarted={secondCard.getStarted}
+          />
 
-            <SecondCard
-              img={fifthCard.img}
-              title={fifthCard.title}
-              message={fifthCard.message}
-              getStarted={fifthCard.getStarted}
-            />
+          <SecondCard
+            img={thirdCard.img}
+            title={thirdCard.title}
+            message={thirdCard.message}
+            getStarted={thirdCard.getStarted}
+          />
 
-            <SecondCard
-              img={sixthCard.img}
-              title={sixthCard.title}
-              message={sixthCard.message}
-              getStarted={sixthCard.getStarted}
-            />
-          </div>
+          <SecondCard
+            img={fourthCard.img}
+            title={fourthCard.title}
+            message={fourthCard.message}
+            getStarted={fourthCard.getStarted}
+          />
+
+          <SecondCard
+            img={fifthCard.img}
+            title={fifthCard.title}
+            message={fifthCard.message}
+            getStarted={fifthCard.getStarted}
+          />
+
+          <SecondCard
+            img={sixthCard.img}
+            title={sixthCard.title}
+            message={sixthCard.message}
+            getStarted={sixthCard.getStarted}
+          />
         </div>
       </main>
 
-      {/* Footer */}
       <footer>
         <div>
           <img src={light_logo} alt="white-logo" />
@@ -82,22 +85,15 @@ function LandingPage() {
   );
 }
 
-// const secondCards = () => {
-//   return <div>
-
-//   </div>;
-// };
-
-// Second Card
 const secondCard = {
-  img: './asset/icon-animation.svg',
+  img: animationIcon,
   title: 'Animation',
   message:
     'Learn the latest animation techniques to create stunning motion design and captivate your audience.',
   getStarted: 'Get Started',
 };
 const thirdCard = {
-  img: './icon-design.svg',
+  img: designIcon,
   title: 'Design',
   message:
     'Create beautiful, usable interfaces to help shape the future of how the web looks.',
@@ -105,7 +101,7 @@ const thirdCard = {
 };
 
 const fourthCard = {
-  img: './icon-photography.svg',
+  img: photographyIcon,
   title: 'Photography',
   message:
     'Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.',
@@ -113,7 +109,7 @@ const fourthCard = {
 };
 
 const fifthCard = {
-  img: './icon-crypto.svg',
+  img: cryptoIcon,
   title: 'Crypto',
   message:
     'All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.',
@@ -121,17 +117,16 @@ const fifthCard = {
 };
 
 const sixthCard = {
-  img: './asset/icon-business.svg',
+  img: businessIcon,
   title: 'Business',
   message:
     'A step-by-step playbook to help you start, scale, and sustain your business without outside investment.',
   getStarted: 'Get Started',
 };
 
-// First Card
 const FirstCard = () => {
   return (
-    <div className="all-cards">
+    <div className="cards all-cards">
       <h4>Check out our most popular courses!</h4>
     </div>
   );
